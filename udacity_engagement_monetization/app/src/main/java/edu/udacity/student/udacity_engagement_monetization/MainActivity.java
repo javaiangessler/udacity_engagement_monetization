9317f6f72ccc23477a8a7e76049f6d5b86bcc86a
Ian Gessler <javaiangessler@gmail.com>
2017-08-28 19:57:29 -0500

    create Android Studio project

---
+++ udacity_engagement_monetization/app/src/main/java/edu/udacity/student/udacity_engagement_monetization/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.udacity.student.udacity_engagement_monetization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
